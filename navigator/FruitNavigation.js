7abc9cb1a2d0130316801dbb62cf77667a80c7ae
HamzaAli <hamzaregardless333@gmail.com>
2021-04-12 15:10:10 +0500

    only Just Naviogation Replace Working is reaming

--- navigator/FruitNavigation.js
+++ navigator/FruitNavigation.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 import Fontisto from 'react-native-vector-icons/Fontisto';
-import Ionicons from 'react-native-vector-icons/Ionicons'; 
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'; 
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import Colors from '../constants/Colors';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
@@ -12,28 +14,42 @@ import LoginScreen from '../screens/LoginScreen';
 import CreateAccountScreen from '../screens/CreateAccountScreen';
 import ForgotPasswordScreen from '../screens/ForgotPasswordScreen';
 import VerificationScreen from '../screens/VerificationScreen';
-import PickUpAndDeliveredScreen from '../screens/OrderPicupAndDeliveredScreen';
+import PickUpAndDeliveredScreen from '../screens/AcceptedOrderScreen';
 import PickedUpOrdersScreen from '../screens/PickedUpOrdersScreens';
 import OrderCompeletedScreen from '../screens/OrderCompeletedScreen.js';
 import NewOrdersScreen from '../screens/NewOrdersScreen';
-import OrderPickedUpAndDeliverd from '../screens/OrderPicupAndDeliveredScreen';
+import OrderPickedUpAndDeliverd from '../screens/AcceptedOrderScreen';
 
 const Stack = createStackNavigator();
 
 const MyStack = () => {
   return (
     <Stack.Navigator initialRouteName="splashScreen">
-      <Stack.Screen name="splashScreen" component={SplashScreen} options={{headerShown: false}} />
-      <Stack.Screen name="loginScreen" component={LoginScreen} />
+      <Stack.Screen
+        name="splashScreen"
+        component={SplashScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="loginScreen"
+        options={{headerShown: false}}
+        component={LoginScreen}
+      />
       <Stack.Screen
         name="createAccountScreen"
         component={CreateAccountScreen}
+        options={{headerShown: false}}
       />
       <Stack.Screen
         name="forgotPasswordScreen"
         component={ForgotPasswordScreen}
+        options={{headerTitle: ''}}
       />
-      <Stack.Screen name="verficationScreen" component={VerificationScreen} />
+      <Stack.Screen
+        name="verficationScreen"
+        component={VerificationScreen}
+        options={{headerTitle: ''}}
+      />
       <Stack.Screen
         name="pickedUpOrderScreen"
         component={PickedUpOrdersScreen}
@@ -41,41 +57,55 @@ const MyStack = () => {
       <Stack.Screen
         name="orderCompeletedScreen"
         component={OrderCompeletedScreen}
+        options={{headerShown: false}}
       />
       <Stack.Screen
         name="pickedUpAndDeliveryedScreen"
         component={PickUpAndDeliveredScreen}
       />
-      <Stack.Screen name="newOrdersScreen" 
-      component={NewOrdersScreen} />
+      <Stack.Screen name="newOrdersScreen" component={NewOrdersScreen} />
 
-
-
-      <Stack.Screen name="AppTabsScreen" options={{headerShown: false}} component={AppTabsScreen} />
+      <Stack.Screen
+        name="AppTabsScreen"
+        options={{headerShown: false}}
+        component={AppTabsScreen}
+      />
     </Stack.Navigator>
   );
 };
 
-// const HomeStack = createStackNavigator();
-// const HomeStackScreen = () => (
-//   <HomeStack.Navigator>
-//     <HomeStack.Screen name="Home" component={CreateAccountScreen} />
-//   </HomeStack.Navigator>
-// );
+const OrderPickedUpAndDeliverdStack = createStackNavigator();
+const OrderPickedUpAndDeliverdStackScreen = () => (
+  <OrderPickedUpAndDeliverdStack.Navigator>
+    <OrderPickedUpAndDeliverdStack.Screen
+      name="Home"
+      options={{headerTitle: 'ID : 345 347 23'}}
+      component={OrderPickedUpAndDeliverd}
+    />
+  </OrderPickedUpAndDeliverdStack.Navigator>
+);
 
 const NewOrderStack = createStackNavigator();
 const NewOrderStackScreen = () => (
   <NewOrderStack.Navigator>
-    <NewOrderStack.Screen name="Order" options={{headerTitle:'Song'}} component={NewOrdersScreen} />
+    <NewOrderStack.Screen
+      name="New Order"
+      options={{headerTitle: "New Order's"}}
+      component={NewOrdersScreen}
+    />
   </NewOrderStack.Navigator>
 );
 
-// const LoginStack = createStackNavigator();
-// const LoginStackScreen = () => (
-//   <LoginStack.Navigator>
-//     <LoginStack.Screen name="Login" component={LoginScreen} />
-//   </LoginStack.Navigator>
-// );
+const PickedUpOrdersScreenStack = createStackNavigator();
+const PickedUpOrdersScreenStackScreen = () => (
+  <PickedUpOrdersScreenStack.Navigator>
+    <PickedUpOrdersScreenStack.Screen
+      name="Picked Up Order"
+      options={{headerTitle: 'ID : 347 e34 34'}}
+      component={PickedUpOrdersScreen}
+    />
+  </PickedUpOrdersScreenStack.Navigator>
+);
 
 const AppTabs = createBottomTabNavigator();
 
@@ -84,34 +114,58 @@ const AppTabsScreen = () => (
     tabBarOptions={{
       showLabel: true,
       showIcon: true,
+      activeTintColor: Colors.buttonColor,
+      style: {height: 55, paddingBottom: 10},
     }}>
-    
     <AppTabs.Screen
       options={{
         headerShown: true,
-        headerTitle: 'Category',
-        tabBarIcon: () => <Fontisto name="bell"  size={24}/>,
+        tabBarIcon: ({focused}) => (
+          <Fontisto
+            name="bell"
+            size={24}
+            color={focused ? Colors.buttonColor : Colors.textGrayColor}
+          />
+        ),
       }}
       name="New Orders"
       component={NewOrderStackScreen}
     />
     <AppTabs.Screen
       options={{
-        tabBarIcon: () => <Fontisto name="bell"  size={24}/>,
+        tabBarIcon: ({focused}) => (
+          <MaterialCommunityIcons
+            name="bike"
+            size={24}
+            color={focused ? Colors.buttonColor : Colors.textGrayColor}
+          />
+        ),
       }}
       name="Accepted"
-      component={OrderPickedUpAndDeliverd }
+      component={OrderPickedUpAndDeliverdStackScreen}
     />
     <AppTabs.Screen
       options={{
-        tabBarIcon: () => <Ionicons name="cart-outline"  size={24}/>,
+        tabBarIcon: ({focused}) => (
+          <Ionicons
+            name="cart-outline"
+            size={24}
+            color={focused ? Colors.buttonColor : Colors.textGrayColor}
+          />
+        ),
       }}
       name="Picked Up"
-      component={PickedUpOrdersScreen}
+      component={PickedUpOrdersScreenStackScreen}
     />
     <AppTabs.Screen
       options={{
-        tabBarIcon: () => <MaterialCommunityIcons name="account-outline"  size={24}/>,
+        tabBarIcon: ({focused}) => (
+          <MaterialCommunityIcons
+            name="account-outline"
+            size={24}
+            color={focused ? Colors.buttonColor : Colors.textGrayColor}
+          />
+        ),
       }}
       name="Account"
       component={LoginScreen}
@@ -119,4 +173,4 @@ const AppTabsScreen = () => (
   </AppTabs.Navigator>
 );
 
-export default () => (<MyStack /> );
+export default () => <MyStack />;
--- screens/CreateAccountScreen.js
+++ screens/CreateAccountScreen.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { View, StyleSheet, Button } from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, Button} from 'react-native';
 
 import Paddings from '../constants/Paddings';
 import Colors from '../constants/Colors';
@@ -9,83 +9,69 @@ import MyButton from '../components/MyButton';
 import HeadingText from '../components/HeadingText';
 import ErrorText from '../components/RedColorText';
 
-
 const CreateAccountScreen = props => {
+  return (
+    <View style={styles.screen}>
+      <View style={styles.container}>
+        <View style={styles.headingContainer}>
+          <HeadingText>Create New Account</HeadingText>
 
-    return (
-        <View style={styles.screen}>
-
-        <View style={styles.container}>
-
-            <View style={styles.headingContainer}>
-            <HeadingText>
-                Create New Account
-    </HeadingText>
-
-            <ErrorText>Please Enter Correct Information!</ErrorText>
-
-
-            </View>
-
-            
-            
-            <MyInputText
-                style={{
-                    marginTop: 30
-                }}
-
-                placeholder='Full Name' />
-
-            <MyInputText
-                style={styles.inputTextMargins}
-                placeholder='Email' />
-
-            <MyInputText
-                style={styles.inputTextMargins}
-                placeholder='Password' />
-
-            <MyInputText
-                style={styles.inputTextMargins}
-                placeholder='Phone NO' />
-
-            <MyInputText
-                style={styles.inputTextMargins}
-                placeholder='Your Location' />
-
-            <MyInputText
-                style={styles.inputTextMargins}
-                placeholder='Date of Birth' />
-
-            <MyButton onPress={() => { }} style={{
-                marginTop: 20
-            }}>
-                Continue
-    </MyButton>
-
-            </View>
-
-
-
+          <ErrorText>Please Enter Correct Information!</ErrorText>
         </View>
-    )
-}
+
+        <MyInputText
+          style={{
+            marginTop: 30,
+          }}
+          placeholder="Full Name"
+        />
+
+        <MyInputText style={styles.inputTextMargins} placeholder="Email" />
+
+        <MyInputText style={styles.inputTextMargins} placeholder="Password" />
+
+        <MyInputText style={styles.inputTextMargins} placeholder="Phone NO" />
+
+        <MyInputText
+          style={styles.inputTextMargins}
+          placeholder="Your Location"
+        />
+
+        <MyInputText
+          style={styles.inputTextMargins}
+          placeholder="Date of Birth"
+        />
+
+        <MyButton
+          onPress={() => {
+            props.navigation.navigate('forgotPasswordScreen');
+          }}
+          style={{
+            marginTop: 20,
+          }}>
+          Continue
+        </MyButton>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        backgroundColor: Colors.backgroundColor,
-        padding: Paddings.normalPadding,
-    },
-    headingContainer:{
-        marginTop: '10%',
-    },
-    container:{
-        alignItems:'center',
-        justifyContent: 'center'
-    },
-    inputTextMargins: {
-        marginTop: 10
-    }
+  screen: {
+    flex: 1,
+    backgroundColor: Colors.backgroundColor,
+    padding: Paddings.normalPadding,
+  },
+  headingContainer: {
+    marginTop: '10%',
+  },
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputTextMargins: {
+    marginTop: 10,
+  },
 });
 
 export default CreateAccountScreen;
